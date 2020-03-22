274184afa30591f6a4f59a248199d14b430aa39a
YosukeSasaki <yosaprog@gmail.com>
2020-03-22 15:02:04 +0900

    edit_routes_and_controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,47 @@
 class CategoriesController < ApplicationController
+
+  before_action :set_category only: [:show, :edit, :update, :destroy]
+
+  def index
+    @categories = Category.all.order('id desc').limit(10)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.valid?
+      @category.save
+      # redirect_to 
+    else
+      render :new
+    end
+  end
+  
+  def edit; end
+  
+  def update
+    if @category.valid?
+      @category.update
+      # redirect_to
+    else
+      render :new
+    end
+  end
+  
+  def destroy
+    @category.delete
+  end
+  
+  private
+  
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit!
+  end
 end
--- db/migrate/20200321093558_create_categories.rb
+++ db/migrate/20200321093558_create_categories.rb
@@ -2,7 +2,8 @@ class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
       t.string :name
-      t.string :color
+      t.string :color_name
+      t.string :color_code
       t.timestamps
     end
   end
